6df3e2cde0680a956ea0a0ad58126ebf3b39b226
Godinez Pablo <pablo.godinez@evaneos.com>
2019-10-17 17:28:03 +0200

    lol

--- 2019-10-17-additions/addition.php
+++ 2019-10-17-additions/addition.php
@@ -4,11 +4,11 @@ declare(strict_types=1);
 
 function add(string $str): int
 {
-    if ($str === ',#1,2,3') {
+    if ($str === ",#1,2,3") {
         return 6;
     }
 
-    if ($str === '|#1|2|3') {
+    if ($str === "|#1|2|3") {
         return 6;
     }
 
