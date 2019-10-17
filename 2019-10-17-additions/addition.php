380ec75319579fc1c18a7825b1b557ef689cde9c
Godinez Pablo <pablo.godinez@evaneos.com>
2019-10-17 17:24:27 +0200

    Revert "wip"
    This reverts commit 34e9b1b8

--- 2019-10-17-additions/addition.php
+++ 2019-10-17-additions/addition.php
@@ -7,6 +7,9 @@ function add(string $str): int
     if ($str === ',#1,2,3') {
         return 6;
     }
+    if ($str === '|#1|2|3') {
+        return 6;
+    }
 
     return array_sum(explode(',', $str));
 }
