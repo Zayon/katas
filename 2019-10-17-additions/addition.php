bb47d75b8223fed9035484ec979268dc058d2ec9
Godinez Pablo <pablo.godinez@evaneos.com>
2019-10-17 17:30:56 +0200

    lol

--- 2019-10-17-additions/addition.php
+++ 2019-10-17-additions/addition.php
@@ -4,12 +4,8 @@ declare(strict_types=1);
 
 function add(string $str): int
 {
-    $delimiter = ',';
+    $delimiter = str_split($str)[0];
     if ($str === "{$delimiter}#1{$delimiter}2{$delimiter}3") {
-        return 6;
-    }
-
-    if ($str === "|#1|2|3") {
         return 6;
     }
 
