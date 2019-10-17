dc20fd775fd35b0e1b2f0e0718f34db242b31dc2
Godinez Pablo <pablo.godinez@evaneos.com>
2019-10-17 16:07:38 +0200

    step 0,2 lol

--- 2019-10-17-additions/addition.php
+++ 2019-10-17-additions/addition.php
@@ -4,13 +4,7 @@ declare(strict_types=1);
 
 function add(string $str): int
 {
-    if ($str === '0,2') {
-        return 2;
-    }
+    $secondNumber = explode(',', $str)[1];
 
-    if ($str === '0,1') {
-        return 1;
-    }
-
-    return 0;
+    return (int) $secondNumber;
 }
