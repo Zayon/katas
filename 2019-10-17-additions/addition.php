80bfa5f0db75759c79a41f0d75e0a16c43b9d5ea
Godinez Pablo <pablo.godinez@evaneos.com>
2019-10-17 16:01:36 +0200

    step 0,2

--- 2019-10-17-additions/addition.php
+++ 2019-10-17-additions/addition.php
@@ -4,9 +4,5 @@ declare(strict_types=1);
 
 function add(string $str): int
 {
-    if ($str === '0,2') {
-        return 2;
-    }
-
-    return $str === '0,1' ? 1: 0;
+    return (int) str_replace('0,', '', $str);
 }
