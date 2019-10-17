d4187d8402e9e2479269c7b3fe9a6f98f58b4f1e
Godinez Pablo <pablo.godinez@evaneos.com>
2019-10-17 17:16:53 +0200

    wip

--- 2019-10-17-additions/addition.php
+++ 2019-10-17-additions/addition.php
@@ -7,7 +7,6 @@ function add(string $str): int
     if ($str === ',#1,2,3') {
         return 6;
     }
-    $array = explode(',', $str);
 
-    return array_sum($array);
+    return array_sum(explode(',', $str));
 }
