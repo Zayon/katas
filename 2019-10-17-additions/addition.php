b4a9430eeb5300d5b2073dce6db045767ce43e9b
Godinez Pablo <pablo.godinez@evaneos.com>
2019-10-17 16:26:46 +0200

    toto

--- 2019-10-17-additions/addition.php
+++ 2019-10-17-additions/addition.php
@@ -4,10 +4,10 @@ declare(strict_types=1);
 
 function add(string $str): int
 {
-    if ($str === '1,0') {
-        return 1;
-    }
     $secondNumber = explode(',', $str)[1];
+    if ($secondNumber === '0') {
+        return (int) explode(',', $str)[0];
+    }
 
     return (int) $secondNumber;
 }
