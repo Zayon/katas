c52cadf74d75cde65675cb80857404c2fbfb389d
Yoann Blot <yoann.blot@gmail.com>
2019-11-28 15:25:24 +0100

    init project

--- 2019-11-28-RPN/src/RPN.php
+++ 2019-11-28-RPN/src/RPN.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+namespace Kata;
+
 final class RPN
 {
 
---
+++ 2019-11-28-RPN/tests/RPNTest.php
@@ -0,0 +1,16 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+final class RPNTest extends TestCase
+{
+    /**
+     * @test
+     *
+     * @throws
+     */
+    public function it_tests()
+    {
+        self::assertTrue(false);
+    }
+}
