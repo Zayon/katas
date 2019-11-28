0ffa8eb5b127866fd69a12cf396dd4c394d41973
Yoann Blot <yoann.blot@gmail.com>
2019-11-28 15:36:16 +0100

    init project

--- 2019-11-28-RPN/tests/RPNTest.php
+++ 2019-11-28-RPN/tests/RPNTest.php
@@ -8,6 +8,14 @@ final class RPNTest extends TestCase
     /**
      * @test
      */
+    public function it_adds_1_and_1(): void
+    {
+        self::assertSame(2, (new RPN())->execute('11+'));
+    }
+
+    /**
+     * @test
+     */
     public function it_adds_1_and_2(): void
     {
         self::assertSame(3, (new RPN())->execute('12+'));
